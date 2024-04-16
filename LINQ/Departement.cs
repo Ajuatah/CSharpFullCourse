bc341df1ff4b59dd33ecbfa2a8c64c395d7b58d0
Ajuatah <ajuatahcodingarena@gmail.com>
2024-04-16 19:40:34 +0100

    added flattening nested collection using slect manay

---
+++ LINQ/Departement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Department
+    {
+        public List<Employee> Employees { get; set; }
+    }
+
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,47 @@
+﻿using LINQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class Program
+{
+    public static List<Department> GetDepartments()
+    {
+        // Simulated data for demonstration
+        List<Department> departments = new List<Department>
+        {
+            new Department
+            {
+                Employees = new List<Employee>
+                {
+                    new Employee { Name = "John", Id = 1 },
+                    new Employee { Name = "Alice", Id = 2 }
+                }
+            },
+            new Department
+            {
+                Employees = new List<Employee>
+                {
+                    new Employee { Name = "Bob", Id = 3 },
+                    new Employee { Name = "Emma", Id = 4 }
+                }
+            }
+        };
+        return departments;
+    }
+
+    public static void Main(string[] args)
+    {
+        var employees = GetDepartments()
+            .SelectMany(dept => dept.Employees)
+            .ToList();
+
+        // Displaying the result
+        Console.WriteLine("Employees:");
+        foreach (var employee in employees)
+        {
+            Console.WriteLine($"Name: {employee.Name}, ID: {employee.Id}");
+        }
+    }
+}
